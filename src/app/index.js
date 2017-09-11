b35acf4498f4115fb44201349217f093ac9b510f
Doug VonMoser <dvonmoser@cars.com>
2017-09-11 08:50:30 -0500

    hacky hide/show toolbar, bug when expanding to tablet after clicks

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,22 @@
 import './styles/main.scss';
 import './assets/save_the_date.jpg';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    // mobile toolbar toggle
+    let button = document.querySelector('.toolbar-toggle');
+    let ex = document.querySelector('.ex');
+    let showenElement = document.querySelector('.tool-wrapper');
+
+    ex.addEventListener('click', function(e){
+        showenElement.style.display = 'none';
+    });
+
+    button.addEventListener('click', function(e){
+        showenElement.style.display = 'block';
+    });
+    // end mobile toolbar toggle
+
+
+
+});
