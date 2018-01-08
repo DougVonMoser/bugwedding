cf8e34830e984cb169034ea4b7f6d4ad6e0c7416
Doug VonMoser <dvonmoser@cars.com>
2018-01-07 21:08:50 -0600

    starting fresh

--- src/app/index.js
+++ src/app/index.js
@@ -4,19 +4,6 @@ import Rx from 'rxjs';
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    // mobile toolbar toggle
-    let button = document.querySelector('.toolbar-toggle');
-    let ex = document.querySelector('.ex');
-    let showenElement = document.querySelector('.tool-wrapper');
-    Rx.Observable.fromEvent(ex, 'click')
-        .subscribe(event => {
-            showenElement.style.display = 'none';
-        });
-
-    Rx.Observable.fromEvent(button, 'click')
-        .subscribe(event => {
-            showenElement.style.display = 'block';
-        });
-    // end mobile toolbar toggle
+    console.log('get this thing done')
 
 });
