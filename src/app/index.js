d757e959e55c4d7e6c2f27aee1048f6a0cfabe80
Doug VonMoser <dvonmoser@cars.com>
2018-01-16 10:15:53 -0600

    stable state i suppose, brad and chris would be proud

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ import './assets/dancing.jpg';
 import './assets/hands.jpg';
 import './assets/musical_notes.jpg';
 import './assets/sitting_bench.jpg';
+import './assets/face_placeholder.jpg';
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,9 @@ module.exports = {
                 use: [
                     {
                         loader: 'file-loader',
-                        options: {}
+                        options: {
+                            name: '[name].[ext]'
+                        }
                     }
                 ]
             }
